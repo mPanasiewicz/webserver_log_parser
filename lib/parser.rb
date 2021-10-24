341a583d5b8a8cdc0f7332ca1e8b2bc5d90cc111
mpanasiewicz <maciej.panasiewicz@reevoo.com>
2021-10-24 23:00:49 +0100

    Prapre data for uniq counts

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,21 +8,34 @@ class Parser
   end
 
   def parse(file_path)
-    urls = []
+    urls = Hash.new { |hash, key| hash[key] = [] }
+
     file = File.open(file_path)
     file.readlines.each do |line|
-      urls << line.split(' ')[0]
+      splited_line = line.split(' ')
+      urls[splited_line[0]] << splited_line[1]
     end
     file.close
-    { most_viewed_pages: count_and_sort_pages(urls) }
+    # { most_viewed_pages: count_and_sort_pages(urls) }
+    {
+      most_viewed_pages: count_and_sort_pages(urls),
+      most_uniq_pages: count_and_sort_uniq_pages(urls)
+    }
   end
 
   private
 
   def count_and_sort_pages(urls)
-    urls.tally
-        .sort_by { |_key, value| value }
-        .reverse
-        .to_h
+    urls_amount = {}
+    urls.each do |url, addr|
+      urls_amount[url] = addr.length
+    end
+    urls_amount.sort_by { |_key, value| value }
+               .reverse
+               .to_h
+  end
+
+  def count_and_sort_uniq_pages(_urls)
+    {}
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -8,13 +8,20 @@ RSpec.describe Parser do
 
     context 'when the file is not empty' do
       let(:file) { '/Users/maciejpanasiewicz/Downloads/SP Ruby Test/ruby_app/webserver.log' }
-      let(:most_viewed_pages) do
-        { most_viewed_pages: { '/about/2' => 90, '/contact' => 89, '/index' => 82, '/about' => 81,
-                               '/help_page/1' => 80, '/home' => 78 } }
+      let(:result) do
+        {
+          most_viewed_pages: { '/about/2' => 90, '/contact' => 89, '/index' => 82, '/about' => 81,
+                               '/help_page/1' => 80, '/home' => 78 },
+          most_uniq_pages: {}
+        }
       end
 
-      it 'returns top 10 urls amount' do
-        expect(parser).to eq(most_viewed_pages)
+      it 'returns urls amount in descending order' do
+        expect(parser[:most_viewed_pages]).to eq(result[:most_viewed_pages])
+      end
+
+      it 'returns amount of uniq urls' do
+        expect(parser[:most_uniq_pages]).to eq({})
       end
     end
   end
